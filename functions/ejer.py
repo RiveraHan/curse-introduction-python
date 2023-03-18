5435effdf8485de99f1df9088bd2cfab62435595
Hanzell Rivera <hanzellrivera95@gmail.com>
2023-03-18 12:05:44 -0600

    docstring

--- functions/ejer.py
+++ functions/ejer.py
@@ -1,9 +1,17 @@
+"""The function that validates if is palindrome"""
+
+
 def is_palindrome(text):
+    """is_palindrome is the function that validates whether is palindrome
+
+        Args:
+            text(string) text to validate for is_palindrome
+    """
     text_clean = text.strip().lower()
     text_reverse = text_clean[::-1]
 
-    for i, t in enumerate(text_clean):
-        if t != text_reverse[i]:
+    for i, char in enumerate(text_clean):
+        if char != text_reverse[i]:
             return False
     return True
 
